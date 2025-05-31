0b64db1f2cd47b30c140e42d1ea85c998ee5c17b
Felipe Franco <felipejf739@hotmail.com>
2025-05-31 15:19:53 -0300

    adfgh

--- API-PokemonCards/API-PokemonCards/src/main/java/com/example/API_PokemonCards/exception/RegraNegocioException.java
+++
@@ -1,7 +0,0 @@
-package com.example.API_PokemonCards.exception;
-
-public class RegraNegocioException extends RuntimeException{
-    public RegraNegocioException(String msg) {
-        super(msg);
-    }
-}
--- API-PokemonCards/API-PokemonCards/src/main/java/com/example/API_PokemonCards/model/repository/SerieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.API_PokemonCards.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.API_PokemonCards.model.entity.Serie;
-public interface SerieRepository extends JpaRepository<Serie, Long> {
-
-}
