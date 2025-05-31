e45cdf41bdedc6fcce015201941676419d58ede7
Felipe Franco <felipejf739@hotmail.com>
2025-05-31 15:20:14 -0300

    sfdfht

---
+++ API-PokemonCards/API-PokemonCards/src/test/java/com/example/API_PokemonCards/ApiPokemonCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API_PokemonCards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPokemonCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
